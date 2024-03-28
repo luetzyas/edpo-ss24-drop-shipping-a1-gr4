ce7c1f9a5b73636d401121c57d4e2344b6991504
@stefanmhsg <st.meier.4@gmail.com>
2024-03-28 11:36:01 +0100

    Mqtt implementation of VGR Service for Order topic

---
+++ kafka/java/vgr-camunda/src/main/java/io/flowing/retail/vgr/domain/FactoryOrderState.java
@@ -0,0 +1,13 @@
+package io.flowing.retail.vgr.domain;
+
+import lombok.Data;
+
+@Data
+public class FactoryOrderState {
+    private String state;
+    private String timestamp;
+    private String type;
+    private String topic;
+
+
+}
