7b0c4945dc68b28a89d20269766b078fcb67be2c
@stefanmhsg <st.meier.4@gmail.com>
2024-03-30 23:36:56 +0100

    assembling order item after checkout

--- kafka/java/checkout/src/main/java/io/flowing/retail/checkout/flow/OrderPlacedAdapter.java
+++ kafka/java/checkout/src/main/java/io/flowing/retail/checkout/flow/OrderPlacedAdapter.java
@@ -1,5 +1,6 @@
 package io.flowing.retail.checkout.flow;
 
+import io.flowing.retail.checkout.domain.Customer;
 import io.flowing.retail.checkout.domain.Order;
 import io.flowing.retail.checkout.messages.Message;
 import io.flowing.retail.checkout.messages.MessageSender;
@@ -8,15 +9,27 @@ import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Component
 public class OrderPlacedAdapter implements JavaDelegate {
     @Autowired
     private MessageSender messageSender; // Autowired to use Spring's KafkaTemplate
 
+    private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(OrderPlacedAdapter.class);
+
     @Override
     public void execute(DelegateExecution execution) throws Exception {
-        Order order = (Order) execution.getVariable("order");
-        Message<Order> message = new Message<>("OrderPlacedEvent", order);
-        messageSender.send(message); // Serialize and send the order as a Kafka message
+
+        try {
+            // Read order from process variables
+            Order order = (Order) execution.getVariable("order");
+
+            Message<Order> message = new Message<>("OrderPlacedEvent", order);
+            messageSender.send(message); // Serialize and send the order as a Kafka message
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- kafka/java/checkout/src/main/java/io/flowing/retail/checkout/flow/ProcessOrderAdapter.java
+++ kafka/java/checkout/src/main/java/io/flowing/retail/checkout/flow/ProcessOrderAdapter.java
@@ -6,19 +6,44 @@ import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Component
 public class ProcessOrderAdapter implements JavaDelegate {
 
+    private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(ProcessOrderAdapter.class);
+
+
     @Override
     public void execute(DelegateExecution execution) throws Exception {
-        // Retrieve form data from process variables
-        String customerName = (String) execution.getVariable("customerName");
-        String customerAddress = (String) execution.getVariable("customerAddress");
-        // Process the order details here
-        // For simplicity, just setting an example order object as a process variable
+
+        // Create a new order
+        logger.info("ProcessOrderAdapter: Creating order");
         Order order = new Order();
-        order.setCustomer(new Customer(customerName, customerAddress));
-        // Add items to the order...
+
+        // Set customer details
+        Customer customer = new Customer();
+        customer.setName((String) execution.getVariable("customerName"));
+        customer.setAddress((String) execution.getVariable("customerAddress"));
+        order.setCustomer(customer);
+        logger.info("ProcessOrderAdapter: Customer details set to " + customer.getName() + " " + customer.getAddress());
+
+        // Aggregate items by articleId
+        Map<String, Integer> aggregatedItems = new HashMap<>();
+        for (int i = 1; i <= 3; i++) { // 3 items Red/Blue/White
+            String articleId = (String) execution.getVariable("articleId" + i);
+            Integer amount = (Integer) execution.getVariable("amount" + i);
+
+            if (articleId != null && amount != null) {
+                aggregatedItems.merge(articleId, amount, Integer::sum);
+            }
+        }
+
+        // Add aggregated items to the order
+        aggregatedItems.forEach(order::addItem);
+        logger.info("ProcessOrderAdapter: Items added to order " + order.getItems());
+
         execution.setVariable("order", order); // Store the order object for the next task
     }
 }
