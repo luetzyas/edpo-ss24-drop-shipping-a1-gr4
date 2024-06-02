f8bdbd95fd664e362820cc4471c27a76ed4fe08f
@stefanmhsg <st.meier.4@gmail.com>
2024-06-02 16:49:46 +0200

    add topic auto creation

--- kafka/java/vgr-camunda/src/main/java/io/flowing/retail/vgr/messages/MessageSender.java
+++ kafka/java/vgr-camunda/src/main/java/io/flowing/retail/vgr/messages/MessageSender.java
@@ -1,8 +1,11 @@
 package io.flowing.retail.vgr.messages;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.errors.TopicExistsException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.config.TopicBuilder;
@@ -10,6 +13,9 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
 /**
  * Helper to send messages, currently nailed to Kafka, but could also send via AMQP (e.g. RabbitMQ) or
@@ -48,6 +54,7 @@ public class MessageSender {
   }
   public void sendToKafka(String topic, String payload, String type) {
     try {
+      createTopicIfNotExists(topic);
       ProducerRecord<String, String> record = new ProducerRecord<>(topic, payload);
       if (type != null) {
         record.headers().add("type", type.getBytes(StandardCharsets.UTF_8));
@@ -58,5 +65,24 @@ public class MessageSender {
       throw new RuntimeException("Could not send message to Kafka: " + e.getMessage(), e);
     }
   }
+  private void createTopicIfNotExists(String topicName) throws ExecutionException, InterruptedException {
+    Properties config = new Properties();
+    config.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaTemplate.getProducerFactory().getConfigurationProperties().get("bootstrap.servers"));
 
+    try (AdminClient adminClient = AdminClient.create(config)) {
+      if (!topicExists(adminClient, topicName)) {
+        NewTopic newTopic = new NewTopic(topicName, 1, (short) 1);
+        adminClient.createTopics(Collections.singleton(newTopic)).all().get();
+        System.out.println("Topic created: " + topicName);
+      } else {
+        System.out.println("Topic already exists: " + topicName);
+      }
+    } catch (TopicExistsException e) {
+      System.out.println("Topic already exists (handled in catch): " + topicName);
+    }
+  }
+
+  private boolean topicExists(AdminClient adminClient, String topicName) throws ExecutionException, InterruptedException {
+    return adminClient.listTopics().names().get().contains(topicName);
+  }
 }
